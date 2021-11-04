37f1702fb2de4da66b05e46e41b6c458bf0563e6
Mark P Thomas <markums@gmail.com>
2021-11-03 23:41:20 -0600

    Final alterations for etl

--- deployment/aws/2a_loginAWS_server.sh
+++ deployment/aws/2a_loginAWS_server.sh
@@ -1,1 +1,1 @@
-ssh -i "atelier-sequoia-server.cer" ubuntu@ec2-18-119-123-42.us-east-2.compute.amazonaws.com
+ssh -i "atelier-sequoia-server.cer" ubuntu@ec2-18-219-94-56.us-east-2.compute.amazonaws.com
--- server/db/postgres/etl/extract/csvToJson.js
+++ server/db/postgres/etl/extract/csvToJson.js
@@ -15,8 +15,8 @@ const getFilePath = (filename, dryRun) => {
   return path.resolve(initialPath, filename);
 }
 
-const lineNumberMultiple = 0; // For _to_characteristics: 8; 15, for _to_photos: 0
-const lineNumberMuplipleSpread = 1; // for _to_photos: 3
+const lineNumberMultiple = 12; // For _to_characteristics: (12)
+const lineNumberMultipleSpread = 4;
 module.exports.parseCsvFileToJson = (filename, callbackDB, dryRun = false, maxChunkSize = 10000, maxCollectionRun = 100) => {
   return new Promise((resolve, reject) => {
     const filePath = getFilePath(filename, dryRun);
@@ -38,7 +38,7 @@ module.exports.parseCsvFileToJson = (filename, callbackDB, dryRun = false, maxCh
         write: (json, encoding, callback) => {
           // console.log('Piping JSON', json);
           lineNumber++;
-          if ((lineNumberMultiple * 1000000) < lineNumber && lineNumber <= (lineNumberMultiple + lineNumberMuplipleSpread) * 1000000) { // Current cap since memory issues
+          if ((lineNumberMultiple * 1000000) < lineNumber && lineNumber <= (lineNumberMultiple + lineNumberMultipleSpread) * 1000000) { // Current cap since memory issues
             queue.push(json);
             if (queue.length >= currentChunkSize) {
               console.log(`Writing JSON batch to database up to line ${lineNumber}`);
